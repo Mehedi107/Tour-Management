d5fdce085237705e0675a0dcdea027dbdec86042
Mehedi Hasan <mehedi.hsn.dev.@gmail.com>
2025-08-03 21:06:06 +0600

    Error Handlers for Server, UnhandledRejection, UncaughtException

--- src/server.ts
+++ src/server.ts
@@ -24,3 +24,44 @@ const startServer = async () => {
 };
 
 startServer();
+
+/* 
+✅ unhandledRejection error (if developer forget to handle any promise error)
+✅ uncaughtException error (if developer forget to handle any local error like declared any variable but do not use it)
+✅ signal termination error
+*/
+
+// 🔴 unhandled rejection error
+process.on('unhandledRejection', error => {
+  console.log('Unhandled Rejection caught. Server is shutting down...', error);
+
+  if (server) {
+    server.close(() => process.exit(1));
+  }
+
+  process.exit(1);
+});
+// Promise.reject(new Error('🔴 I forgot to catch this promise...'));
+
+// 🔴 uncaughtException error
+process.on('uncaughtException', error => {
+  console.log('uncaughtException caught. Server is shutting down...', error);
+
+  if (server) {
+    server.close(() => process.exit(1));
+  }
+
+  process.exit(1);
+});
+// throw new Error('🔴 I forgot to handle local error');
+
+// 🔴 SIGTERM error
+process.on('SIGTERM', () => {
+  console.log('SIGTERM caught. Server is shutting down...');
+
+  if (server) {
+    server.close(() => process.exit(1));
+  }
+
+  process.exit(1);
+});
